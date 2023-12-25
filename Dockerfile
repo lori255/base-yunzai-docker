21fbd69c003bc8580300d37a2f470e20fb862371
abu <abu@youxiake.com>
2023-12-25 10:09:17 +0800

    云崽通用镜像

---
+++ Dockerfile
@@ -0,0 +1,81 @@
+FROM busybox:latest AS resource
+ADD docker-entrypoint.sh /res/entrypoint.sh
+
+RUN dos2unix /res/entrypoint.sh \
+    && chmod +x /res/entrypoint.sh
+
+
+FROM node:lts-bullseye-slim AS runtime
+
+ARG BUNDLE_FFMPEG true
+ARG BUNDLE_POETRY true
+ARG USE_APT_MIRROR true
+ARG USE_NPM_MIRROR true
+ARG USE_PYPI_MIRROR true
+
+RUN export BUNDLE_FFMPEG=${BUNDLE_FFMPEG:-true} \
+    && export BUNDLE_POETRY=${BUNDLE_POETRY:-true} \
+    && export USE_APT_MIRROR=${USE_APT_MIRROR:-true} \
+    && export USE_NPM_MIRROR=${USE_NPM_MIRROR:-true} \
+    && export USE_PYPI_MIRROR=${USE_PYPI_MIRROR:-true} \
+    \
+    && ((test "$USE_APT_MIRROR"x = "true"x \
+    && sed -i "s/deb.debian.org/mirrors.ustc.edu.cn/g" /etc/apt/sources.list) || true) \
+    && apt-get update \
+    && apt-get upgrade -y \
+    && apt-get install -y wget xz-utils dos2unix \
+    && ((test "$BUNDLE_FFMPEG"x = "true"x \
+    && wget https://johnvansickle.com/ffmpeg/builds/ffmpeg-git-$(dpkg --print-architecture)-static.tar.xz \
+    && mkdir -p /res/ffmpeg \
+    && tar -xvf ./ffmpeg-git-$(dpkg --print-architecture)-static.tar.xz -C /res/ffmpeg --strip-components 1 \
+    && cp /res/ffmpeg/ffmpeg /usr/bin/ffmpeg \
+    && cp /res/ffmpeg/ffprobe /usr/bin/ffprobe) || true) \
+    \
+    && apt-get update \
+    && apt-get upgrade -y \
+    && apt-get install -y curl wget gnupg git fonts-wqy-microhei xfonts-utils chromium fontconfig libxss1 libgl1 vim \
+    && apt-get autoremove \
+    && apt-get clean \
+    \
+    && fc-cache -f -v \
+    \
+    && git config --global --add safe.directory '*' \
+    && git config --global pull.rebase false \
+    && git config --global user.email "2539939333@qq.com" \
+    && git config --global user.name "lori" \
+    \
+    && _NPM_MIRROR_FLAG="" \
+    && if [ "$USE_NPM_MIRROR"x = "true"x ]; then _NPM_MIRROR_FLAG="--registry=https://registry.npmmirror.com"; fi \
+    && npm install pnpm -g $_NPM_MIRROR_FLAG \
+    \
+    && ((test "$BUNDLE_POETRY"x = "true"x \
+    && apt-get update \
+    && apt-get install -y python3-pip python3-venv \
+    && apt-get autoremove \
+    && apt-get clean \
+    && ln -s /usr/bin/python3 /usr/bin/python \
+    && POETRY_HOME=$HOME/venv-poetry \
+    && python -m venv $POETRY_HOME \
+    && _PYPI_MIRROR_FLAG="" \
+    && if [ "$USE_PYPI_MIRROR"x = "true"x ]; then _PYPI_MIRROR_FLAG="-i https://pypi.tuna.tsinghua.edu.cn/simple"; fi \
+    && $POETRY_HOME/bin/pip install --upgrade pip setuptools $_PYPI_MIRROR_FLAG \
+    && $POETRY_HOME/bin/pip install poetry $_PYPI_MIRROR_FLAG \
+    && ln -s $POETRY_HOME/bin/poetry /usr/bin \
+    && poetry config virtualenvs.in-project true) || true) \
+    \
+    && rm -rf /var/cache/* \
+    && rm -rf /tmp/*
+
+
+FROM runtime AS prod
+
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
+    PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium
+
+RUN mkdir -p /app/Yunzai
+
+COPY --from=resource /res/entrypoint.sh /app/entrypoint.sh
+
+WORKDIR /app/Yunzai
+
+ENTRYPOINT ["/app/entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,139 @@
+#!/usr/bin/env bash
+
+set +e
+
+GreenBG="\\033[42;37m"
+YellowBG="\\033[43;37m"
+BlueBG="\\033[44;37m"
+Font="\\033[0m"
+
+Version="${BlueBG}[版本]${Font}"
+Info="${GreenBG}[信息]${Font}"
+Warn="${YellowBG}[提示]${Font}"
+
+WORK_DIR="/app/Yunzai"
+MIAO_PLUGIN_PATH="/app/Yunzai/plugins/miao-plugin"
+GUOBA_PLUGIN_PATH="/app/Yunzai/plugins/Guoba-Plugin"
+YUNZAI_REPO_URL=${YUNZAI_REPO_URL}
+
+if [[ ! -d "$HOME/.ovo" ]]; then
+    mkdir ~/.ovo
+fi
+
+if [[ -z $YUNZAI_REPO_URL ]]; then
+    echo -e "\n ================ \n ${Warn} ${YellowBG} 未设置环境变量 YUNZAI_REPO_URL ${Font} \n ================ \n"
+    exit 1
+fi
+
+echo -e "\n ================ \n ${Info} ${GreenBG} 拉取 Yunzai 更新 ${Font} \n ================ \n"
+cd $WORK_DIR
+if [ ! -d $MIAO_PLUGIN_PATH"/.git" ]; then
+    echo -e "\n ${Warn} ${YellowBG} 检测到云崽目前没有安装，开始自动下载 ${Font} \n"
+    git clone --depth=1 $YUNZAI_REPO_URL .
+fi
+
+if [[ -z $(git status -s) ]]; then
+    echo -e " ${Warn} ${YellowBG} 当前工作区有修改，尝试暂存后更新。${Font}"
+    git add .
+    git stash
+    git pull origin master --allow-unrelated-histories --rebase
+    git stash pop
+else
+    git pull origin master --allow-unrelated-histories
+fi
+
+if [[ ! -f "$HOME/.ovo/yunzai.ok" ]]; then
+    set -e
+    echo -e "\n ================ \n ${Info} ${GreenBG} 更新 Miao-Yunzai 运行依赖 ${Font} \n ================ \n"
+    pnpm install
+    touch ~/.ovo/yunzai.ok
+    set +e
+fi
+
+echo -e "\n ================ \n ${Version} ${BlueBG} Yunzai 版本信息 ${Font} \n ================ \n"
+git log -1 --pretty=format:"%h - %an, %ar (%cd) : %s"
+if [ ! -d $MIAO_PLUGIN_PATH"/.git" ]; then
+    echo -e "\n ${Warn} ${YellowBG} 检测到没有安装miao-plugin，开始自动下载 ${Font} \n"
+    git clone --depth=1 https://gitee.com/yoimiya-kokomi/miao-plugin.git ./plugins/miao-plugin/
+fi
+
+if [ -d $MIAO_PLUGIN_PATH"/.git" ]; then
+    echo -e "\n ================ \n ${Info} ${GreenBG} 拉取 喵喵插件 更新 ${Font} \n ================ \n"
+    cd $MIAO_PLUGIN_PATH
+    if [[ -n $(git status -s) ]]; then
+        echo -e " ${Warn} ${YellowBG} 当前工作区有修改，尝试暂存后更新。${Font}"
+        git add .
+        git stash
+        git pull origin master --allow-unrelated-histories --rebase
+        git stash pop
+    else
+        git pull origin master --allow-unrelated-histories
+    fi
+
+    if [[ ! -f "$HOME/.ovo/miao.ok" ]]; then
+        set -e
+        echo -e "\n ================ \n ${Info} ${GreenBG} 更新 喵喵插件 运行依赖 ${Font} \n ================ \n"
+        pnpm install
+        touch ~/.ovo/miao.ok
+        set +e
+    fi
+
+    echo -e "\n ================ \n ${Version} ${BlueBG} 喵喵插件版本信息 ${Font} \n ================ \n"
+    git log -1 --pretty=format:"%h - %an, %ar (%cd) : %s"
+fi
+
+if [ ! -d $GUOBA_PLUGIN_PATH"/.git" ]; then
+
+    echo -e "\n ${Warn} ${YellowBG} 检测到没有安装Guoba-Plugin，开始自动下载 ${Font} \n"
+    git clone --depth=1 https://gitee.com/guoba-yunzai/guoba-plugin.git ./plugins/Guoba-Plugin/
+fi
+
+if [ -d $GUOBA_PLUGIN_PATH"/.git" ]; then
+    echo -e "\n ================ \n ${Info} ${GreenBG} 拉取 Guoba-Plugin 插件更新 ${Font} \n ================ \n"
+    cd $GUOBA_PLUGIN_PATH
+
+    if [[ -n $(git status -s) ]]; then
+        echo -e " ${Warn} ${YellowBG} 当前工作区有修改，尝试暂存后更新。${Font}"
+        git add .
+        git stash
+        git pull origin master --allow-unrelated-histories --rebase
+        git stash pop
+    else
+        git pull origin master --allow-unrelated-histories
+    fi
+
+    if [[ ! -f "$HOME/.ovo/guoba.ok" ]]; then
+        set -e
+        echo -e "\n ================ \n ${Info} ${GreenBG} 更新 Guoba-Plugin 插件运行依赖 ${Font} \n ================ \n"
+        pnpm add multer body-parser jsonwebtoken -w
+        touch ~/.ovo/guoba.ok
+        set +e
+    fi
+
+    echo -e "\n ================ \n ${Version} ${BlueBG} Guoba-Plugin 插件版本信息 ${Font} \n ================ \n"
+
+    git log -1 --pretty=format:"%h - %an, %ar (%cd) : %s"
+fi
+
+set -e
+
+cd $WORK_DIR
+
+echo -e "\n ================ \n ${Info} ${GreenBG} 初始化 Docker 环境 ${Font} \n ================ \n"
+
+if [ -f "./config/config/redis.yaml" ]; then
+    sed -i 's/127.0.0.1/redis/g' ./config/config/redis.yaml
+    echo -e "\n  修改Redis地址完成~  \n"
+fi
+
+echo -e "\n ================ \n ${Info} ${GreenBG} 启动 Yunzai ${Font} \n ================ \n"
+
+set +e
+pnpm start
+EXIT_CODE=$?
+
+if [[ $EXIT_CODE != 0 ]]; then
+	echo -e "\n ================ \n ${Warn} ${YellowBG} 启动 Yunzai 失败 ${Font} \n ================ \n"
+	tail -f /dev/null
+fi
+/app/Yunzai/node_modules/pm2/bin/pm2 logs
